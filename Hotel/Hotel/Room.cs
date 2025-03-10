2d4db164bb5dc6749d3851b14f4ffeeda3334aab
brauck <109923715+brauck@users.noreply.github.com>
2025-03-10 13:18:33 +0300

    update course

---
+++ Hotel/Hotel/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public enum ROOMQUANTITY {'Single','Double','Triple','Quadruple'};
+    public class Room
+    {
+        public uint RoomId { get; set; }
+        public uint ClientId { get; set; }        
+    }
+}
