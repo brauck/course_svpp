19d2bbfc7784dbb3076f21373acd4775f023cb1c
brauck <109923715+brauck@users.noreply.github.com>
2025-03-13 02:48:45 +0300

    finish raw

--- Hotel/Hotel/MainWindow.xaml.cs
+++ Hotel/Hotel/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace Hotel
         public MainWindow()
         {
             InitializeComponent();
+            datePickerIn.SelectedDate = DateTime.Now;
+            datePickerOut.SelectedDate = DateTime.Now;
             loaddata();
         }
 
@@ -32,19 +34,19 @@ namespace Hotel
             Room room = new();
             Client client = new();
             EditWindow editWindow = new EditWindow(room, client);
-            if (editWindow.ShowDialog() == false) return;
+            if (editWindow.ShowDialog() == false) return;            
             loaddata();
         }
 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
+        //private void btnDelete_Click(object sender, RoutedEventArgs e)
+        //{
 
-        }
+        //}
 
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
-        {
+        //private void btnEdit_Click(object sender, RoutedEventArgs e)
+        //{
 
-        }
+        //}
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
@@ -75,6 +77,58 @@ namespace Hotel
             {
                 conn.Close();
             }
-        }        
+        }
+
+        private void btnShowPeriod_Click(object sender, RoutedEventArgs e)
+        {
+            ShowReport();
+        }
+
+        private void ShowReport()
+        {
+            //MainWindow newWindow = new MainWindow();
+            //Application.Current.MainWindow = newWindow;
+            //newWindow.Show();
+            //this.Close();
+
+            DateTime ddateIn = (DateTime)datePickerIn.SelectedDate;
+            DateTime ddateOut = (DateTime)datePickerOut.SelectedDate;
+
+            string format = "yyyy-MM-dd";
+            string dateIn = ddateIn.ToString(format);
+            string dateOut = ddateOut.ToString(format);
+
+            List.Content = $"Отчет о заявках на период с {dateIn} по {dateOut}";
+
+            DataTable dt = new DataTable();
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(
+                    $"select room.id, rooms_quantity, client.name, order_date_at, order_date_end" +
+                    $" from order_clients_rooms" +
+                    $" join  client on  client.id = order_clients_rooms.client_id" +
+                    $" join  room on  room.id = order_clients_rooms.room_id" +
+                    $" where order_date_at >= '{dateIn}' and order_date_at <= '{dateOut}'" +
+                    $" order by order_date_at desc", conn);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+
+                adp.Fill(dt);
+                dGrid.ItemsSource = dt.DefaultView;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            loaddata();
+        }
     }
 }
